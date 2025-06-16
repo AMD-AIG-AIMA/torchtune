16179899f38beed2b7e4b3b858cc19b7e3567f9b
Niko Sakkinen <niko.sakkinen@amd.com>
2025-06-16 07:50:03 +0000

    Fixed a bug in padding

--- torchtune/modules/transforms/_transforms.py
+++ torchtune/modules/transforms/_transforms.py
@@ -172,9 +172,8 @@ class VisionCrossAttentionMask(Transform):
             # to a single image, so text tokens attend to all the image's tokens.
             # The mask is text_seq_len x mask_image_size if defined, otherwise
             # it uses current text/image sequence lengths.
-            if is_flash_attn_available():
-                n_pads = (64 - (image_seq_len % 64)) % 64
-                mask = torch.zeros(text_seq_len, image_seq_len+n_pads, dtype=torch.bool)
+            n_pads = (64 - (image_seq_len % 64)) % 64 if is_flash_attn_available() else 0
+            mask = torch.zeros(text_seq_len, image_seq_len+n_pads, dtype=torch.bool)
             mask[start:end, :image_seq_len] = True
             masks.append(mask)
 
